2df974d40e15625098374ce0f7d5ce75798cf787
LehruAngela <angelalehru@gmail.com>
2017-11-24 12:35:39 +0300

    Corrected imports

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 
 from app import create_app
-from config import app_config
+from instance.config import app_config
 
 config_name = app_config['development']
 app = create_app(config_name)
