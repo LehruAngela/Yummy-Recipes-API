b784326b367da15acef5eb878302d4cb7be0ef7e
LehruAngela <angelalehru@gmail.com>
2017-11-24 11:24:00 +0300

    Updated db configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,26 @@
+from flask import Flask
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
 # local import
 from instance.config import app_config
 
+"""POSTGRESTEST = {
+'user': 'postgres',
+'db': 'recipe_db',
+'host': 'localhost',
+'port': '5432',
+}"""
 # initialize sql-alchemy
 db = SQLAlchemy()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development']) # app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    with app.app_context():
+    db.create_all()
 
     return app
