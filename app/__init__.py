0427dd0a05b70a2eb15bc6abd406bb362b45ee08
LehruAngela <angelalehru@gmail.com>
2017-12-15 13:05:57 +0300

    Update file

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app(config_name):
     from app.models.recipe import Recipe
     from app.models.recipeAuth import RecipeApp
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config['development']) # app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config[config_name]) #app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
