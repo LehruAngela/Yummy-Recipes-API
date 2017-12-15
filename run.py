6a41b28b89990671a566dc2fc31619374d7ca018
LehruAngela <angelalehru@gmail.com>
2017-12-15 13:05:26 +0300

    Updated file

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from app import create_app
 from instance.config import app_config
 
-config_name = app_config['development']
+config_name = os.getenv('FLASK_CONFIG') #app.config['development']
 app = create_app(config_name)
 
 if __name__ == '__main__':
