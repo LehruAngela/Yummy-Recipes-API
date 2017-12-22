160e1e295b4b4c9eb5dd5fc9873aa5d07377d131
Angela Lehru <angelalehru@Angelas-MacBook-Pro.local>
2017-12-22 13:21:12 +0300

    Refactored code with wraps func tools

--- app/categories/views.py
+++ app/categories/views.py
@@ -1,66 +1,77 @@
 from . import category_api
+from functools import wraps
 from flask import Blueprint, make_response, request, jsonify, url_for
 from app.models.category import Category
 from app.models.recipe import Recipe
 from app.models.recipeAuth import RecipeApp
-from app.recipes.views import recipe_api
-from sqlalchemy import and_
+
+def auth(func):
+
+    @wraps(func)
+    def user_login(*args, **kwargs):
+        # Get the access token from the header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+            # Attempt to decode the token and get the User ID
+            user_id = RecipeApp.decode_token(access_token)
+            if not isinstance(user_id, str):
+                # Handle the request if the user is authenticated"""
+                return func(user_id, *args, **kwargs)
+            return None
+        return None
+    return user_login
+
 
 @category_api.route('/categories/', methods=['GET','POST'])
-#@category_api.route('/categories/<str:q>', methods=['GET'])
-def create_and_view_categories():
+@auth
+def create_and_view_categories(user_id):
     # retrieves/adds categories from/to the database
-    # Get the access token from the header
-    auth_header = request.headers.get('Authorization')
-    access_token = auth_header.split(" ")[1]
+    if request.method == "POST":
+        category_name = str(request.data.get('category_name', ''))
+        #if validate.validate_name(category_name) == True:
+        print("------------>", category_name)
+        if category_name:
+            print("------------>", category_name)
+            category = Category(category_name=category_name, user_id=user_id)
+            category.save()
+            response = jsonify({
+                'category_id': category.category_id,
+                'category_name': category.category_name,
+                'date_created': category.date_created,
+                'date_modified': category.date_modified,
+                'recipes': url_for('recipe_api.recipes', category_id=category.category_id, _external=True),
+                'created_by' : user_id
+                })
+            return make_response(response), 201
 
-    if access_token:
-        # Attempt to decode the token and get the User ID
-        user_id = RecipeApp.decode_token(access_token)
-        print ("--------->", user_id)
-        if not isinstance(user_id, str):
-            # Handle the request if the user is authenticated
-            if request.method == "POST":
-                category_name = str(request.data.get('category_name', ''))
-                if category_name:
-                    #category_name = category_name.title()
-                    category = Category(category_name=category_name, user_id=user_id)
-                    category.save()
-                    response = jsonify({
-                        'category_id': category.category_id,
-                        'category_name': category.category_name,
-                        'date_created': category.date_created,
-                        'date_modified': category.date_modified,
-                        'recipes': url_for('recipe_api.recipes', category_id=category.category_id, _external=True),
-                        'created_by' : user_id
-                        })
-                    return make_response(response), 201
+    else:
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 5))
+        q = str(request.args.get('q', '')).title()
+        #GET all the categories created by this user
+        #categories = Category.query.filter_by(user_id=user_id, category_name=q)
+        categories = Category.query.filter(Category.user_id==user_id).filter(Category.category_name.like('%'+q+'%')).paginate(page, per_page, False)
 
-            else:
-                page = int(request.args.get('page', 1))
-                per_page = int(request.args.get('per_page', 5))
-                q = str(request.args.get('q', '')).title()
-                #GET all the categories created by this user
-                #categories = Category.query.filter_by(user_id=user_id, category_name=q)
-                categories = Category.query.filter(Category.user_id==user_id).filter(Category.category_name.like('%'+q+'%')).paginate(page, per_page)
+        results = []
 
-                results = []
+        for category in categories.items:
+            obj = {
+                'category_id': category.category_id,
+                'category_name': category.category_name,
+                'date_created': category.date_created,
+                'date_modified': category.date_modified,
+                'recipes': url_for('recipe_api.recipes', category_id=category.category_id, _external=True),
+                'created_by' : user_id
+            }
+            results.append(obj)
 
-                for category in categories.items:
-                    obj = {
-                        'category_id': category.category_id,
-                        'category_name': category.category_name,
-                        'date_created': category.date_created,
-                        'date_modified': category.date_modified,
-                        'recipes': url_for('recipe_api.recipes', category_id=category.category_id, _external=True),
-                        'created_by' : user_id
-                    }
-                    results.append(obj)
-
-                return make_response(jsonify(results)), 200
+        return make_response(jsonify(results)), 200
 
 
 @category_api.route('/categories/<int:category_id>', methods=['GET', 'PUT', 'DELETE'])
+@auth
 def category_edit_and_delete(category_id, **kwargs):
     # retrieve a category using it's ID
     category = Category.query.filter_by(category_id=category_id).first()
