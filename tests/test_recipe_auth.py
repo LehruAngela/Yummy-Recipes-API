1c5f796ee819b1e077773cb9783699d78d9cd338
Angela Lehru <angelalehru@Angelas-MacBook-Pro.local>
2018-01-07 21:36:37 +0300

    Rectified status code

--- tests/test_recipe_auth.py
+++ tests/test_recipe_auth.py
@@ -7,18 +7,11 @@ class TestRecipeApp(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.user = {'email': 'Gela@gela.com',
+        self.user = {'email': 'Gela1@gela.com',
                      'password': '123'}
 
         with self.app.app_context():
             db.create_all()
-
-    def tearDown(self):
-        """teardown all initialized variables."""
-        with self.app.app_context():
-            # drop all tables
-            db.session.remove()
-            db.drop_all()
 
     def test_register(self):
         """Test API can register users (POST request)"""
@@ -30,3 +23,10 @@ class TestRecipeApp(unittest.TestCase):
         self.test_register()
         res = self.client().post('/api-v1/auth/login', data=self.user)
         self.assertEqual(res.status_code, 200)
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
