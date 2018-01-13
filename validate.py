a9e00188129b30ffe8654dda078d1539c3871260
LehruAngela <angelalehru@gmail.com>
2018-01-13 10:32:08 +0300

    Modified validations in validate.py

--- validate.py
+++ validate.py
@@ -1,14 +1,22 @@
 import re #regular expressions
-
-def validate_password(password):
-    if re.match(r'[a-zA-Z0-9]', password):
-        return "True"
+from flask import make_response, jsonify
 
 def validate_email(email):
     if re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', email):
         return "True"
 
-def validate_name(name):
-    name = str(name)
-    if re.match(r'[A-Za-z]', name): 
+def validate_password(password):
+    if len(password) >= 6:
         return "True"
+
+def validate_name(name):
+    return "True"
+    # name = str(name)
+    # if re.match(r'(?=.*[A-Za-z]*$)', name):
+    #     return "True"
+    #     if not re.match(r"(^[ ]*$)", name):
+    #         return "True"
+    #     response = {'message':'A space is not a name'}
+    #     return make_response(jsonify(response)), 401
+    # response = {'message': 'Name should be in alphabetical'}
+    # return make_response(jsonify(response)), 401
