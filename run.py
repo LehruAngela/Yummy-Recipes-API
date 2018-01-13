c8f2a673abc6e3cca601e163f4b4464b382c0c26
LehruAngela <angelalehru@gmail.com>
2018-01-13 10:33:52 +0300

    Added swagger template

--- run.py
+++ run.py
@@ -1,10 +1,30 @@
 import os
 
+from flask import render_template, redirect
+from flasgger import Swagger
 from app import create_app
 from instance.config import app_config
 
 config_name = os.getenv('FLASK_CONFIG') #app.config['development']
 app = create_app(config_name)
+
+swag= Swagger(app,
+   template={
+       "info": {
+       "title": "Yummy Recipes api-v1",
+       "description": "API that registers and logs in a user so as to use the features and functionality of yummy recipes."},
+       "securityDefinitions":{
+           "TokenHeader": {
+               "type": "apiKey",
+               "name": "Authorization",
+               "in": "header"
+           }
+       }
+   })
+@app.route("/")
+def main():
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
