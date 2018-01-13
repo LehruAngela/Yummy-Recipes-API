22bd5d9502be73c9290e473f8e35e3d80b8e9049
LehruAngela <angelalehru@gmail.com>
2018-01-13 10:27:35 +0300

    Implemented logout and reset password functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from . import auth_blueprint
 import validate
 from flask.views import MethodView
 from flask import Blueprint, make_response, request, jsonify
-from app.models.recipeAuth import RecipeApp
+from app.models.recipeAuth import RecipeApp, ExpiredToken
+from flask_bcrypt import Bcrypt
 
 class RegistrationView(MethodView):
     """This class registers a new user."""
@@ -51,29 +52,30 @@ class RegistrationView(MethodView):
                 # Register the user
                 email = request.data['email']
                 password =  request.data['password']
-                if validate.validate_email(email) == "True" and validate.validate_password(password) == "True":
-                    user = RecipeApp(email=email, password=password)
-                    user.save()
-
-                    response = {
-                        'message': 'You registered successfully. Please log in.'
-                    }
-                    # return a response notifying the user that they registered successfully
-                    return make_response(jsonify(response)), 201
+                security_question = request.data['security_question']
+                security_answer = request.data['security_answer']
+                if not email or not password or not security_question or not security_answer:
+                    response = {'message': 'All fields are required'}
+                    return make_response(jsonify(response)), 400
+                if validate.validate_email(email) == "True":
+                    if validate.validate_password(password) == "True":
+                        user = RecipeApp(email=email, password=password, security_question=security_question, security_answer=security_answer)
+                        user.save()
+                        response = {'message': 'You registered successfully. Please log in.'}
+                        # return a response notifying the user that they registered successfully
+                        return make_response(jsonify(response)), 201
+                    response = {'message': 'Password is short. Enter a password longer than 6 characters'}
+                    return make_response(jsonify(response)), 400
+                response = {'message': 'Invalid email! A valid email should in this format name@gmail.com' }
+                return make_response(jsonify(response)), 401
             except Exception as e:
                 # An error occured, therefore return a string message containing the error
-                response = {
-                    'message': str(e)
-                }
-                return make_response(jsonify(response)), 500
+                response = {'message': str(e)}
+                return make_response(jsonify(response)), 400
         else:
             # There is an existing user.
-            response = {
-                'message': 'User already exists. Please login.'
-            }
-
+            response = {'message': 'User already exists. Please login.'}
             return make_response(jsonify(response)), 409
-
 
 
 class LoginView(MethodView):
@@ -139,7 +141,7 @@ class LoginView(MethodView):
                     response = {
                         'message': 'You logged in successfully.',
                         'access_token': access_token.decode()
-                    }
+                     }
                     return make_response(jsonify(response)), 200
             else:
                 # User does not exist. Therefore, we return an error message
@@ -181,7 +183,7 @@ class LogoutView(MethodView):
                 # Handle the request if the user is authenticated"""
                 expired_token = ExpiredToken(token=access_token)
                 expired_token.save()
-                return jsonify({'message': 'Your have been logged out.'}),201
+                return jsonify({'message': 'You have been logged out.'}),200
             else:
                 message = user_id
                 response = {'message': message}
@@ -189,10 +191,43 @@ class LogoutView(MethodView):
         else:
             return jsonify({'message': 'please provide a  valid token'})
 
+
+class ResetPasswordView(MethodView):
+    def post(self):
+        """
+        Reset Password
+        ---
+        tags:
+          - Users
+        parameters:
+          - in: body
+            name: body
+            required: true
+            type: string
+        responses:
+          200:
+            description: You've reset your password successfully
+        """
+        #register user
+        email = request.data['email']
+        new_password = request.data['new_password']
+        security_question = request.data['security_question']
+        security_answer = request.data['security_answer']
+        user = RecipeApp.query.filter_by(email=email,security_answer=security_answer).first()
+        if user:
+            user.password = Bcrypt().generate_password_hash(new_password).decode()
+            user.save()
+            response = {'message': 'Your password has been reset'}
+            return make_response(jsonify(response)), 201
+        response = {'message': 'wrong email or wrong security answer, try again'}
+        return make_response(jsonify(response)), 401
+
 # Define the API resource
 registration_view = RegistrationView.as_view('registration_view')
 login_view = LoginView.as_view('login_view')
 logout_view = LogoutView.as_view('logout_view')
+reset_password_view = ResetPasswordView.as_view('reset_password_view')
+
 
 # Define the rule for the registration url --->  /auth/register
 # Then add the rule to the blueprint
@@ -216,3 +251,10 @@ auth_blueprint.add_url_rule(
     view_func=logout_view,
     methods=['POST']
 )
+
+# Define the rule for the reset password url --->  /auth/reset_password
+# Then add the rule to the blueprint
+auth_blueprint.add_url_rule(
+    '/auth/reset_password',
+    view_func=reset_password_view,
+    methods=['POST'])
