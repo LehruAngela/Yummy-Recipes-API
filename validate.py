6a26f2e8379583ccd172b443c68abf36188cf81f
LehruAngela <angelalehru@gmail.com>
2018-01-16 19:34:01 +0300

    Added special characters in validate file

--- validate.py
+++ validate.py
@@ -8,15 +8,17 @@ def validate_email(email):
 def validate_password(password):
     if len(password) >= 6:
         return "True"
+        if re.match(r'^[A-Za-z_*.&]*$', name):
+            return "True"
 
 def validate_name(name):
     return "True"
-    # name = str(name)
-    # if re.match(r'(?=.*[A-Za-z]*$)', name):
-    #     return "True"
-    #     if not re.match(r"(^[ ]*$)", name):
-    #         return "True"
-    #     response = {'message':'A space is not a name'}
-    #     return make_response(jsonify(response)), 401
-    # response = {'message': 'Name should be in alphabetical'}
-    # return make_response(jsonify(response)), 401
+    name = str(name)
+    if re.match(r'^[A-Za-z]*$', name):
+        return "True"
+        if not re.match(r"(^[ ]*$)", name):
+            return "True"
+        response = {'message':'A space is not a name'}
+        return make_response(jsonify(response)), 401
+    response = {'message': 'Name should be in alphabetical'}
+    return make_response(jsonify(response)), 401
