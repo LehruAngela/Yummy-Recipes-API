b2636add938ab88dd5cba700eb5715be57b86d6a
LehruAngela <angelalehru@gmail.com>
2018-03-19 22:02:05 +0300

    [ft] Added migrations and refactored Auth Model

--- app/models/recipeAuth.py
+++ app/models/recipeAuth.py
@@ -17,7 +17,7 @@ class RecipeApp(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
-    username = db.Column(db.String(256), nullable=False)
+    username = db.Column(db.String(256))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
